1e42291cb7a9f9b7683c34edcd86f9d6cecc1f80
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2024-02-02 11:45:54 -0300

    fix: make hutch works

---
+++ config/initializers/hutch.rb
@@ -0,0 +1,7 @@
+Hutch::Config.set(:mq_host, ENV['RABBITMQ_HOST'])
+Hutch::Config.set(:mq_port, ENV['RABBITMQ_PORT'])
+Hutch::Config.set(:mq_username, ENV['RABBITMQ_USER'])
+Hutch::Config.set(:mq_password, ENV['RABBITMQ_PASSWORD'])
+
+# Hutch::Logging.logger = Rails.logger
+Hutch.connect
