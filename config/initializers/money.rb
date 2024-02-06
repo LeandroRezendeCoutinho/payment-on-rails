b230ebdcf84a070f4dcf85e0ab5ac4364592e0a4
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2024-02-06 11:13:15 -0300

    chore: remove warning

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -6,6 +6,7 @@ MoneyRails.configure do |config|
   #
   config.default_currency = :BRL
   config.locale_backend = :i18n
+  config.rounding_mode =  BigDecimal::ROUND_HALF_EVEN
   # Set default bank object
   #
   # Example:
