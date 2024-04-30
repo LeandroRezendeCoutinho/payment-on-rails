914028d5d9f4df639146ad088e4bb298cc4f29c6
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2024-04-30 08:34:29 -0300

    chore: add basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,63 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+client1 = ClientService.new.create_new_client
+client1.name = "Client Teste 1"
+client1.save
+
+client2 = ClientService.new.create_new_client
+client2.name = "Client Teste 2"
+client2.save
+
+integration_config = {
+  provider: {
+    name: "Local",
+    baseUrl: "https://localhost:3500",
+    payment_path: "/payments",
+    auth_path: "/auth",
+    auth_method: "POST",
+    credentials: {
+      user: "user",
+      password: "password"
+    }
+  },
+  mapping: [
+    {
+      from: "amount",
+      to: "amount"
+    },
+    {
+      from: "capture",
+      to: "capture"
+    },
+    {
+      from: "status",
+      to: "status"
+    },
+    {
+      from: "order_id",
+      to: "order_id"
+    },
+    {
+      from: "payment_type",
+      to: "payment_type"
+    },
+    {
+      from: "source_type",
+      to: "source_type"
+    }
+  ]
+}
+
+integration1 = Integration.new
+integration1.name = "Integration Teste 1"
+integration1.client = client1
+integration1.config = integration_config
+integration1.save
+
+integration2 = Integration.new
+integration2.name = "Integration Teste 2"
+integration2.client = client2
+integration2.config = integration_config
+integration2.save
